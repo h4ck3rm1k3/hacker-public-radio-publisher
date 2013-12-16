09102660937008596024bd79ec03477feedccd8c
James Michael DuPont <jamesmikedupont@gmail.com>
2013-12-15 18:21:49 -0600

    example

---
+++ tests/example3.sh
@@ -0,0 +1,6 @@
+echo example 
+python ../hacker_public_radio.py --create test3   --series="Testing" --tag=Funky --tag=Soul \
+--short="short descr" \
+--medium="medium lenght  descr" \
+--title="My test show from the command line with a very long title that wont be truncated ......" \
+--shownotes-load  --save-yaml-file=test2.yaml --save-json-file=test2.json
